2eb0f2e5aa2a5edcd3ae7e493c8cd625bad5eb17
Vlad <vlados1998blc@gmail.com>
2018-06-27 21:31:24 +0300

    more files

---
+++ CW2.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/is-this-my-tail/train/python
+
+def correct_tail(body, tail):
+     sub = body.substr(len(body)-len(tail.length)
+        if sub = tai:
+    return True
+        else:
+    return False
