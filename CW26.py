210b4087637ea86e12644b3a8d0a7a5d0330e502
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:53:52 +0300

    Delete CW26.py

--- CW26.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/558fc85d8fd1938afb000014
-
-numbers = [1, 2, 3, 4, 5]
-
-def sum_two_smallest_numbers(numbers):
-    numbers.sort()
-    sum = numbers[0] + numbers[1]
-    return sum
