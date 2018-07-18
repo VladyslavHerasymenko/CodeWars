3c8e4bf5cf4a53363d9bc680d20648fb1c427fd8
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:52:29 +0300

    Delete CW19.py

--- CW19.py
+++
@@ -1,7 +0,0 @@
-# https://www.codewars.com/kata/55225023e1be1ec8bc000390
-
-def greet(name):
-    if name == "Johnny":
-        return "Hello, my love!"
-    else:
-        return "Hello, {name}!".format(name=name)
