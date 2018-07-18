e4d2d75cc4d6968605a51716837726668884d90b
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:52:38 +0300

    Delete CW2.py

--- CW2.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/is-this-my-tail/train/python
-
-def correct_tail(body, tail):
-     sub = body.substr(len(body)-len(tail.length)
-        if sub = tai:
-    return True
-        else:
-    return False
