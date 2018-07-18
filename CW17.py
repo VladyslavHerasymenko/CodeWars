16ba9e89af3ed87e0c26772d1f7bb0b7d1b9d0a2
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:52:04 +0300

    Delete CW17.py

--- CW17.py
+++
@@ -1,6 +0,0 @@
-# https://www.codewars.com/kata/53af2b8861023f1d88000832
-
-def areYouPlayingBanjo(name):
-    if name[0] == "R" or name[0] == "r":
-        return name + " plays banjo"
-    return name + " does not play banjo"
