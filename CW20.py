8a0180caaa409b066a9520db0bd60b4e43c6cf06
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:52:47 +0300

    Delete CW20.py

--- CW20.py
+++
@@ -1,9 +0,0 @@
-# https://www.codewars.com/kata/5a00e05cc374cb34d100000d
-
-def reverse_seq(n):
-    mas = []
-    while n != 0:
-        mas.append(n)
-        n = n - 1 
-        continue
-    return mas
