defbc14d688961a66ea22c6963ca1f326a87ab39
Vladyslav Herasymenko <40401734+VladyslavHerasymenko@users.noreply.github.com>
2018-07-18 19:50:45 +0300

    Delete CW12.py

--- CW12.py
+++
@@ -1,7 +0,0 @@
-# https://www.codewars.com/kata/565f5825379664a26b00007c
-
-def get_size(w,h,d):
-    arr = []
-    arr.append(2 * (w * h + h * d + w * d))
-    arr.append(w * h * d)
-    return arr
